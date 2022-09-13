988ed0a02f2fe7890ef9eafbad43945b4103027b
Nuel-Eneji <enejiemmanueladie@gmail.com>
2022-09-13 10:52:49 -0700

    updated commit 3

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -1,11 +1,13 @@
 #!/usr/bin/python3
+"""Square with size"""
+
+
 class Square:
-    """ A class that defines a square by its size
-    """
+    """Representation of a square"""
+
     def __init__(self, size=0):
-        """ Method to initialize the square object
-        """
-        if not isinstance(size, int):
+        """Instantiation with optional size"""
+        if type(size) != int:
             raise TypeError("size must be an integer")
         elif size < 0:
             raise ValueError("size must be >= 0")
@@ -13,6 +15,5 @@ class Square:
             self.__size = size
 
     def area(self):
-        """ Method that returns the square are of the object
-        """
-        return (self.__size ** 2)
+        """Public instance method that returns the current square area"""
+        return self.__size ** 2
